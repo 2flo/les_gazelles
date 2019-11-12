47c5e24da0418647ba3d2d9f7b4184e3946a00fd
2flo <dianeflozin@gmail.com>
2019-11-12 17:02:53 +0100

    Ajout d'un effet dans la page presentation

--- prestashop_1.7.6.1/themes/child_classic/assets/js/custom.js
+++ prestashop_1.7.6.1/themes/child_classic/assets/js/custom.js
@@ -3,6 +3,7 @@
  * A template should always ship with an empty index.js
  */
 
-$(document).ready(function(){
-    $(".page-subheading").fadeIn("slow", "complete");
-});
+$(document).ready(function(){ //Toujours commencer par cibler le document et à quel moment, ici c'est au chargement//
+    let fade = $(".page-subheading"); //Les titres h3 de la page présentation vont s'afficher sur un fondu au démarrage//
+    fade.fadeIn(3000);
+})
